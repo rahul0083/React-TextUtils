9fc0e4d37899d6738717c2964ea5f5850fb1be23
Rahul Singh <rahulme0083@gmail.com>
2023-02-17 23:49:11 +0530

    update2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { useState } from 'react';
 import './App.css';
 import Alert from './Components/Alert';
-import About from './Components/About';
+//import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
   const [mode,setmode]=useState('light');  
@@ -43,18 +43,19 @@ function App() {
   }
   return (
     <>
-        <Router>
+        {/* <Router> */}
      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
      <Alert alert={alert}/>
      <div className="container my-3" >
-     <Routes>
+     {/* <Routes>
           <Route exact path="/about" element={<About />}>   
           </Route>
           <Route exact path="/" element={ <TextForm showAlert={showAlert} heading="Enter the text to Analyse" mode={mode}  />}>
           </Route>
-        </Routes>
+        </Routes> */}
+        <TextForm showAlert={showAlert} heading="Enter the text to Analyse" mode={mode}  />
         </div>
-        </Router>
+        {/* </Router> */}
         
 
     </>
